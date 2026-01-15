f78380537ca86ecd7e8f338af5a251ab0ba55221
Zohaib <164843889+zobbygit@users.noreply.github.com>
2026-01-15 23:22:11 +0530

    Add allowed origin for Vercel deployment

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const port=process.env.PORT || 4000
 
 await connectDB()
 await connectCloudinary()
-const allowedOrigin=['http://localhost:5173']
+const allowedOrigin=['http://localhost:5173','https://swad-express-ruddy.vercel.app']
 app.post('/stripe',express.raw({type:'application/json'}),stripeWebhooks)
 
 app.use(express.json())
